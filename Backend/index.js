78be3f3218977183188c2bd21c34ca0a6d72fe8e
Uzmakh <urbazoha@gmail.com>
2023-11-14 10:02:48 +0500

    checked the working of other routes

--- Backend/index.js
+++ Backend/index.js
@@ -7,11 +7,21 @@ config(); // Load environment variables from .env file
 const app = express();
 
 app.use(cors())
+
 const PORT = process.env.PORT;
 
+//default rout
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+//other routes
+app.get('/api/v1/facebook', (req, res) => {
+  res.send('Hello Facebook World!')
+})
+app.get('/api/v1/instagram', (req, res) => {
+  res.send('Hello Instagram World!')
+})
+
 
 // Middleware to parse JSON data from requests
 app.use(express.json());
