d26d720f75b81a1973c2f74f37762d038479cc09
Uzmakh <urbazoha@gmail.com>
2023-11-16 18:25:04 +0500

    Set bookRoutes in routes folder and created controller files

--- Backend/routes/baseRoutes.js
+++ Backend/routes/baseRoutes.js
@@ -2,6 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const authRoutes = require('./authRoutes/authRoutes')
+const bookRoutes = require('./bookRoutes/bookRoutes')
+
+// define base routes for your modules
+// book routes
+router.use('/api/v1/', bookRoutes)
 
 // auth routes
 router.use('/api/v1/', authRoutes);
